c9bc7a8625530e95f673b80c46a782fc1fe02d35
eric lugo <elugo25111@gmail.com>
2018-09-13 16:19:18 -0700

    update all

--- lua.d/python.lua
+++
@@ -1,6 +0,0 @@
-py = require 'python'
-
-print_ip = py.import "bl".print_ip
-print_ip('1.2.1.2')
-
--- local status = os.execute("python /root/bl.py 1.2.3.4")
--- scripts/bl.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/python2.7
-
-def print_ip(ip):
-	with open('/root/list.txt', 'a') as f:
-		f.write("{}\n".format(ip))
-
